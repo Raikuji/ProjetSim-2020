c324e161e20c5a95a0a5749bf2002db861d8e9f5
Raikuji <irub.tech@gmail.com>
2020-01-29 11:17:32 +0100

    Ajout modif event

--- src/Immeuble.java
+++ src/Immeuble.java
@@ -58,7 +58,6 @@ public class Immeuble extends Global {
         e = étageLePlusHaut();
         cabine = new Cabine(niveauDuSol());
         e = étage(e.numéro() - 1);
-        echeancier.ajouter(new EvenementFermeturePorteCabine(48));
     }
 
     public void affiche(StringBuilder buffer) {
